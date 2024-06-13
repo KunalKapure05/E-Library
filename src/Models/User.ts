02c9ed448abeb9970e465f04dc8437879bbcc8d1
KunalKapure05 <kapurekunal61@gmail.com>
2024-06-13 14:49:42 +0530

    Added User's Model,Controller and Route

---
+++ src/Models/User.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import {User} from '../Interfaces/User'
+
+const userSchema = new mongoose.Schema<User>({
+
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    }
+
+},
+{
+    timestamps:true
+})
+
+export default mongoose.model<User>('User',userSchema);
+
+
