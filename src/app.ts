08ba72be8c363959e32dc7d9b177cd0d39cac00d
KunalKapure05 <kapurekunal61@gmail.com>
2024-06-13 14:09:23 +0530

    added Express.json for parsing the req body

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from "express";
 
-
-
 import errorHandler from "./middlewares/errorhandler";
 
-
 const app = express();
+
+app.use(express.json())
 
 app.get("/", (req, res,next) => {
   res.json({ message: "Heyyyyyyy" });
