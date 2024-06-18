aa3bbc80e877926b8e02fe9a10539fd2038f8848
KunalKapure05 <kapurekunal61@gmail.com>
2024-06-18 23:42:07 +0530

    Added Book in  Db

--- src/Controllers/bookControllers.ts
+++ src/Controllers/bookControllers.ts
@@ -2,10 +2,13 @@ import { NextFunction,Request,Response } from "express";
 import cloudinary  from "../config/cloudinary";
 import path from "path";
 import createHttpError from "http-errors";
+import fs from "fs"
+import Book from "../Models/Book";
 
 const createBook = async(req:Request,res:Response,next:NextFunction)=>{
 try {
-    
+    const user = req.params.user
+    const {title,genre} = req.body;
     console.log("files",req.files);
 
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
@@ -16,9 +19,9 @@ try {
 
     const fileName = files.coverImage[0].filename;
 
-    const filePath = path.resolve(__dirname,'../../public/data/uploads',fileName)
+    const ImagefilePath = path.resolve(__dirname,'../../public/data/uploads',fileName)
 
-    const uploadedResult = await cloudinary.uploader.upload(filePath,{
+    const uploadedResult = await cloudinary.uploader.upload(ImagefilePath,{
         filename_override:fileName,
         folder: 'book-covers',
         format:coverImageMimeType,
@@ -37,10 +40,26 @@ try {
     })
     console.log("BookuploadResult:",bookFileUpload);
     
+    const newBook = await Book.create({
+        title,
+        genre ,
+        coverImage:uploadedResult.secure_url,
+        file:bookFileUpload.secure_url,
+        author: "666b48a124f54e24ce403256"
+       })
    
     console.log("uploadResult:",uploadedResult);
     
-    return res.json({})
+    //This will delete temp files which are there in Public/data/uploads folder
+    try {
+        await fs.promises.unlink(ImagefilePath)
+        await fs.promises.unlink(bookFilePath)
+        
+    } catch (error) {
+        return next(createHttpError(500,"Error in deleting temp files"))
+    }
+    
+    return res.status(201).json({id: newBook._id})
 } catch (error) {
     console.error(error);
     return next(createHttpError(500,"Error while uploading the files"))
